f0aefd726c08f870264b5b87f4714739bfe4c3f8
eloutassi salma <salma03abb@gmail.com>
2023-07-20 15:37:05 +0100

    tache salma

--- resources/js/src/pages/Beneficiaire/List.jsx
+++ resources/js/src/pages/Beneficiaire/List.jsx
@@ -1,11 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DefaultLayout from '../../components/Layouts/DefaultLayout';
 import { DataTable } from 'mantine-datatable';
-
+import { Link} from '@inertiajs/inertia-react';
+import {  FiEye } from 'react-icons/fi';
+import { encodeID } from '../../keys/Index';
 
 
 const List = ({ beneficiaries, regions, provinces, cities, ecoles }) => {
 
+  const PAGE_SIZES = [10, 20, 30, 50, 100];
+
+  // Les états pour les options de recherche et de pagination
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(PAGE_SIZES[0]);
+  const [initialRecords, setInitialRecords] = useState([]);
+  const [recordsData, setRecordsData] = useState([]);
+
+  // Utilisez useEffect pour mettre à jour initialRecords lorsque beneficiaries change
+  useEffect(() => {
+      if (beneficiaries && beneficiaries.length > 0) {
+          const beneficiaryObj = JSON.parse(beneficiaries[0].dataBeneficiary);
+          const beneficiary = beneficiaryObj.dataBenificiary;
+          setInitialRecords(beneficiary);
+      } else {
+          setInitialRecords([]);
+      }
+  }, [beneficiaries]);
+
+  // Utilisez useEffect pour paginer les données lorsque page ou pageSize change
+  useEffect(() => {
+      const from = (page - 1) * pageSize;
+      const to = from + pageSize;
+      setRecordsData([...initialRecords?.slice(from, to)]);
+  }, [page, pageSize, initialRecords]);
+
+    // Laissez recordsData vide pour afficher le tableau sans données.
+  
 
   const [regionSearch, setRegionSearch] = useState('');
   const [provinceSearch, setProvinceSearch] = useState('');
@@ -56,7 +86,7 @@ const List = ({ beneficiaries, regions, provinces, cities, ecoles }) => {
           </div>
           <div className="datatables">
 
-            <div class="relative overflow-x-auto">
+            {/* <div class="relative overflow-x-auto">
               <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
@@ -106,8 +136,38 @@ const List = ({ beneficiaries, regions, provinces, cities, ecoles }) => {
 
                 </tbody>
               </table>
-            </div>
-
+            </div> */}
+           <DataTable
+                striped
+                className="whitespace-nowrap table-striped"
+                records={recordsData}
+                columns={[
+                    { accessor: 'keys', title: 'Rang' },
+                    { accessor: 'name', title: 'Nom' },
+                    { accessor: 'region', title: 'Region' },
+                    { accessor: 'province', title: 'Province' },
+                    { accessor: 'city', title: 'Ville' },
+                    { accessor: 'school', title: "Lycée" },
+                    {
+                        accessor: 'totalPoints',
+                        title: 'Point',
+                        render: ({ totalPoints }) => <span className='text-white inline-block p-2 rounded hover:text-gray-100 mr-2 bg-danger '>{totalPoints} </span>,
+                    },
+                    {
+                        accessor: 'id',
+                        title: 'Action',
+                        render: ({ id }) => <Link href={route('dashboard.detail.scoring.candidate', encodeID(id))} className='text-white inline-block p-2 rounded hover:text-gray-100 mr-2 bg-blue-600 '><FiEye /></Link>,
+                    },
+                ]}
+                totalRecords={initialRecords?.length}
+                recordsPerPage={pageSize}
+                page={page}
+                onPageChange={(p) => setPage(p)}
+                recordsPerPageOptions={PAGE_SIZES}
+                onRecordsPerPageChange={setPageSize}
+                minHeight={200}
+                paginationText={({ from, to, totalRecords }) => `Showing  ${from} to ${to} of ${totalRecords} entries`}
+            />
           </div>
         </div>
 
