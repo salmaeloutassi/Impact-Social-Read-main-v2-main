c4cf5de46d0bc39b01e80e1329fcdc920351c720
eloutassi salma <salma03abb@gmail.com>
2023-07-20 15:45:54 +0100

    modifications

--- resources/js/src/pages/Beneficiaire/List.jsx
+++ resources/js/src/pages/Beneficiaire/List.jsx
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import DefaultLayout from '../../components/Layouts/DefaultLayout';
 import { DataTable } from 'mantine-datatable';
-<<<<<<< HEAD
 import { Link} from '@inertiajs/inertia-react';
-=======
-import { Link } from '@inertiajs/inertia-react';
->>>>>>> 22a6f0178f92e52383b0d7734f2e1dddd01d2aa4
 import {  FiEye } from 'react-icons/fi';
 import { encodeID } from '../../keys/Index';
 
@@ -90,63 +86,8 @@ const List = ({ beneficiaries, regions, provinces, cities, ecoles }) => {
           </div>
           <div className="datatables">
 
-<<<<<<< HEAD
-            {/* <div class="relative overflow-x-auto">
-              <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-
-                    <th scope="col" class="px-6 py-3">
-                      Region
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                      Province
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                      City
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                      Ecole
-                    </th>
-
-                  </tr>
-                </thead>
-                <tbody>
-
-                  <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <td class="px-6 py-4">
-
-                    </td>
-
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-
-                    </th>
-
-                    <td className='px-6 py-4'>
-
-                    </td>
-
-                    <td class="px-6 py-4">
-
-                    </td>
-                    <td class="px-6 py-4">
-
-                    </td>
-
-
-                  </tr>
-
-
-
-
-                </tbody>
-              </table>
-            </div> */}
+            
            <DataTable
-=======
-           
-          <DataTable
->>>>>>> 22a6f0178f92e52383b0d7734f2e1dddd01d2aa4
                 striped
                 className="whitespace-nowrap table-striped"
                 records={recordsData}
