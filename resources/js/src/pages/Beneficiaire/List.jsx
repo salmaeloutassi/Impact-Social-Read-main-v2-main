4db05de5e3a6539ca6dfc9558c165da1076451f2
eloutassi salma <salma03abb@gmail.com>
2023-07-20 14:39:29 +0100

    List benificiares

--- resources/js/src/pages/Beneficiaire/List.jsx
+++ resources/js/src/pages/Beneficiaire/List.jsx
@@ -86,57 +86,7 @@ const List = ({ beneficiaries, regions, provinces, cities, ecoles }) => {
           </div>
           <div className="datatables">
 
-            {/* <div class="relative overflow-x-auto">
-              <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-
-                    <th scope="col" class="px-6 py-3">
-                      Region
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                      Province
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                      City
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                      Ecole
-                    </th>
-
-                  </tr>
-                </thead>
-                <tbody>
-
-                  <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <td class="px-6 py-4">
-
-                    </td>
-
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-
-                    </th>
-
-                    <td className='px-6 py-4'>
-
-                    </td>
-
-                    <td class="px-6 py-4">
-
-                    </td>
-                    <td class="px-6 py-4">
-
-                    </td>
-
-
-                  </tr>
-
-
-
-
-                </tbody>
-              </table>
-            </div> */}
+           
            <DataTable
                 striped
                 className="whitespace-nowrap table-striped"
